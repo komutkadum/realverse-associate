8ffe0bf1c03b66e537214f6a3dba7d2880b8b45e
komutkadum <komutkadum@gmail.com>
2023-04-10 15:25:07 +0530

    refactor: recognising email from which form

--- components/utility/CTAForm.js
+++ components/utility/CTAForm.js
@@ -60,7 +60,7 @@ function CTAForm() {
         <textarea
           className="hidden"
           name="message"
-          defaultValue="Request a callback"
+          defaultValue={`Request a callback for ${router.asPath}`}
         ></textarea>
         <button className="px-4 relative py-2.5 text-sm btn-bg font-bold rounded-lg bg-amber-500 hover:bg-amber-600">
           {loading ? 'submitting...' : 'Request a Callback'}
